8d8f5802aadd43f39ce377cc0eeeeeef34af840d
vunq <vunqph45621@fpt.edu.vn>
2023-11-14 12:09:36 +0700

    init project

---
+++ src/SinhVien.java
@@ -0,0 +1,103 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ADMIN
+ */
+public class SinhVien {
+   private String ID,Lop,Ten,SDT,NgaySinh,Anh,DiaChi,GioiTinh,Email;
+
+    public SinhVien() {
+    }
+
+    public SinhVien(String ID, String Lop, String Ten, String SDT, String NgaySinh, String Anh, String DiaChi, String GioiTinh, String Email) {
+        this.ID = ID;
+        this.Lop = Lop;
+        this.Ten = Ten;
+        this.SDT = SDT;
+        this.NgaySinh = NgaySinh;
+        this.Anh = Anh;
+        this.DiaChi = DiaChi;
+        this.GioiTinh = GioiTinh;
+        this.Email = Email;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getLop() {
+        return Lop;
+    }
+
+    public void setLop(String Lop) {
+        this.Lop = Lop;
+    }
+
+    public String getTen() {
+        return Ten;
+    }
+
+    public void setTen(String Ten) {
+        this.Ten = Ten;
+    }
+
+    public String getSDT() {
+        return SDT;
+    }
+
+    public void setSDT(String SDT) {
+        this.SDT = SDT;
+    }
+
+    public String getNgaySinh() {
+        return NgaySinh;
+    }
+
+    public void setNgaySinh(String NgaySinh) {
+        this.NgaySinh = NgaySinh;
+    }
+
+    public String getAnh() {
+        return Anh;
+    }
+
+    public void setAnh(String Anh) {
+        this.Anh = Anh;
+    }
+
+    public String getDiaChi() {
+        return DiaChi;
+    }
+
+    public void setDiaChi(String DiaChi) {
+        this.DiaChi = DiaChi;
+    }
+
+    public String getGioiTinh() {
+        return GioiTinh;
+    }
+
+    public void setGioiTinh(String GioiTinh) {
+        this.GioiTinh = GioiTinh;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+   
+   
+   
+}
